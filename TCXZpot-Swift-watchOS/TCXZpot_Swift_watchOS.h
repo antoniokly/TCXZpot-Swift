a0281b57128b2fdae32e72d9b2588eca27d847bb
Antonio Yip <antoniokly@users.noreply.github.com>
2020-05-10 22:28:12 +1200

    watchOS target

---
+++ TCXZpot-Swift-watchOS/TCXZpot_Swift_watchOS.h
@@ -0,0 +1,19 @@
+//
+//  TCXZpot_Swift_watchOS.h
+//  TCXZpot-Swift-watchOS
+//
+//  Created by Antonio Yip on 10/05/20.
+//  Copyright © 2020 SweetZpot. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TCXZpot_Swift_watchOS.
+FOUNDATION_EXPORT double TCXZpot_Swift_watchOSVersionNumber;
+
+//! Project version string for TCXZpot_Swift_watchOS.
+FOUNDATION_EXPORT const unsigned char TCXZpot_Swift_watchOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TCXZpot_Swift_watchOS/PublicHeader.h>
+
+
